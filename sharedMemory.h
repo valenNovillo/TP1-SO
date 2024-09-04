7bfc440bd75b198a594b6bb479df643503ec6577
valenNovillo <VANOVILLO@ITBA.EDU.AR>
2024-09-04 10:45:07 -0300

    Update sharedMemory.h
    details

--- sharedMemory.h
+++ sharedMemory.h
@@ -1,9 +1,10 @@
+#include  <semaphore.h>
+
 #define MAX_BUF 500000
 
-typedef struct 
-{
+typedef struct {
     char buf[MAX_BUF];
     sem_t ready_to_read;
     sem_t ready_to_write;
     sem_t mutex;
-}SharedMemory
+} SharedMemory;
